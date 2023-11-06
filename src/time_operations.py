c7252dd4061878f90f601146ce500c003cf3db8c
aliig <aliigleed16@gmail.com>
2023-11-06 12:10:29 -0500

    fix

--- src/time_operations.py
+++ src/time_operations.py
@@ -10,7 +10,7 @@ class TimeTracker:
         self.task_config = task_config
         self.interval = task_config.get("interval", 4)
         self.current_time = datetime.now()
-        self.blackout_start, self.blackout_end = self._get_blackout_period()
+        self.blackout_start, self.blackout_end = self._get_blackout_times()
         self.time_until = None
         self.set_next_time()
 
