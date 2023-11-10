47a05918378ef4be04e7a788caa05451fefaced8
mgleed <aliigleed16@gmail.com>
2023-11-09 19:16:03 -0500

    fix?

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,5 @@
+import os
+
 SLEEP_TIME = 60  # seconds to sleep between server state checks
 SERVER_TIMEOUT = 20  # seconds to wait for server to start or stop before exiting
-OUTPUT_DIRECTORY = "output"
+OUTPUT_DIRECTORY = os.path.dirname("output")
