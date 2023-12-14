30552bc044d56e179169762044dbd277a208daf4
aliig <aliigleed16@gmail.com>
2023-12-14 05:57:22 -0500

    refactor launch options logic

--- src/shell_operations.py
+++ src/shell_operations.py
@@ -76,54 +76,76 @@ def is_server_running(ark_port: int = CONFIG["server"]["port"]) -> bool:
 
 
 def generate_batch_file() -> str:
-    def _server_config_option(key, format_str):
+    """
+    Generates a batch file based on the provided configuration.
+    :return: The path to the generated batch file.
+    """
+
+    def _server_config_option(key: str, format_str: str) -> str | None:
+        """
+        Formats a server configuration option.
+
+        :param key: The key of the configuration option.
+        :param format_str: The format string.
+        :return: The formatted string or None if the key is not found.
+        """
         value = CONFIG["server"].get(key)
         return format_str.format(value) if value else None
 
-    base_arg = os.path.join(
-        CONFIG["server"]["install_path"],
-        "ShooterGame",
-        "Binaries",
-        "Win64",
-        (
+    def _construct_base_arg() -> str:
+        """
+        Constructs the base argument for the command string.
+
+        :return: The base argument string.
+        """
+        exe_name = (
             "AsaApiLoader.exe"
-            if "use_server_api" in CONFIG["server"]
-            and CONFIG["server"]["use_server_api"]
+            if CONFIG["server"].get("use_server_api")
             else "ArkAscendedServer.exe"
-        ),
-    )
-    question_mark_options_list = [
-        CONFIG["server"]["map"],
-        # _server_config_option("ip_address", "MultiHome={}"),
-        # _server_config_option('name', "SessionName=\"{}\""),
-        # _server_config_option("port", "Port={}"),
-        # _server_config_option("query_port", "QueryPort={}"),
-        # _server_config_option("password", "Password={}"),
-        # _server_config_option("max_players", "MaxPlayers={}"),
-        # _server_config_option("admin_password", "ServerAdminPassword={}"),
-        # "RCONEnabled=True",
-        *CONFIG["launch_options"].get("question_mark", []),
-    ]
+        )
+        return os.path.join(
+            CONFIG["server"]["install_path"],
+            "ShooterGame",
+            "Binaries",
+            "Win64",
+            exe_name,
+        )
 
-    question_mark_options = "?".join(filter(None, question_mark_options_list))
+    def _construct_question_mark_options() -> str:
+        """
+        Constructs the question mark options for the command string.
 
-    hyphen_options = " ".join(
-        [f"-{opt}" for opt in CONFIG["launch_options"].get("hyphen", []) if opt]
-        + [
-            f"-mods={','.join(map(str, CONFIG['launch_options'].get('mods', [])))}"
-            if CONFIG["launch_options"].get("mods")
-            else ""
+        :return: The question mark options string.
+        """
+        options = [CONFIG["server"]["map"]]
+        options.extend(CONFIG["launch_options"].get("question_mark", []))
+        return "?".join(filter(None, options))
+
+    def _construct_hyphen_options() -> str:
+        """
+        Constructs the hyphen options for the command string.
+
+        :return: The hyphen options string.
+        """
+        options = [
+            f"-{opt}" for opt in CONFIG["launch_options"].get("hyphen", []) if opt
         ]
-        + [f"-WinLiveMaxPlayers={CONFIG['server']['max_players']}"]
-    )
+        mods = CONFIG["launch_options"].get("mods")
+        if mods:
+            options.append(f"-mods={','.join(map(str, mods))}")
+        options.append(f"-WinLiveMaxPlayers={CONFIG['server']['max_players']}")
+        return " ".join(options)
+
+    base_arg = _construct_base_arg()
+    question_mark_options = _construct_question_mark_options()
+    hyphen_options = _construct_hyphen_options()
 
     cmd_string = f"{base_arg} {question_mark_options} {hyphen_options}"
     logger.debug(f"launch options: {cmd_string}")
     batch_content = f'@echo off\nstart "" {cmd_string}'
 
-    with open(
-        (file_path := os.path.join(OUTDIR, ".start_server.bat")), "w"
-    ) as batch_file:
+    file_path = os.path.join(OUTDIR, ".start_server.bat")
+    with open(file_path, "w") as batch_file:
         batch_file.write(batch_content)
 
     return file_path
