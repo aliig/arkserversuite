0e482033cfdd88164da099b12e31243e5d5643b5
aliig <aliigleed16@gmail.com>
2023-12-22 10:13:27 -0500

    wip

--- src/shell_operations.py
+++ src/shell_operations.py
@@ -1,4 +1,5 @@
 import os
+import psutil
 import subprocess
 import sys
 
@@ -37,42 +38,67 @@ def run_shell_cmd(
 
 
 def kill_server() -> None:
-    run_shell_cmd("taskkill /IM ArkAscendedServer.exe /F", suppress_output=True)
-    run_shell_cmd("taskkill /IM AsaApiLoader.exe /F", suppress_output=True)
+    """
+    Kills the server processes.
+    """
+    for process in psutil.process_iter(["pid", "name"]):
+        if process.info["name"] in ["ArkAscendedServer.exe", "AsaApiLoader.exe"]:
+            process.terminate()
+            logger.debug(
+                f"Terminated process {process.info['name']} with PID {process.info['pid']}"
+            )
 
 
-def get_process_id(expected_port: int) -> int | None:
-    cmd = "netstat -ano"
-    process = run_shell_cmd(cmd, suppress_output=True)
+def get_process_from_port(expected_port: int) -> int | None:
+    """
+    Retrieves the process ID that is using the specified port.
 
-    if process.returncode != 0:
-        logger.error("Error running netstat:", process.stderr)
-        return None
-
-    for line in process.stdout.splitlines():
-        parts = line.split()
-        if len(parts) >= 2 and parts[1].startswith("0.0.0.0:"):
-            port = int(parts[1].split(":")[1])
-            if port == expected_port:
-                process_id = int(parts[-1])
-                logger.debug(f"Found process id {process_id} on port {port}")
-                return process_id
+    :param expected_port: The port number to check.
+    :return: The process ID if found, otherwise None.
+    """
+    for conn in psutil.net_connections(kind="inet"):
+        if conn.laddr.port == expected_port:
+            res = conn.pid
+            logger.debug(f"Process id on port {expected_port} found: {res}")
+            return res
     logger.debug(f"Process id on port {expected_port} not found")
-    # logger.debug(f"output was: {process.stdout}")
     return None
 
 
 def is_server_running(ark_port: int = CONFIG["server"]["port"]) -> bool:
-    if not (pid := get_process_id(ark_port)):
-        return False
+    """
+    Checks if the server is running on the specified port.
 
+    :param ark_port: The port number to check.
+    :return: True if the server is running, False otherwise.
+    """
+    pid = get_process_from_port(ark_port)
+    if pid is None:
+        return False
     try:
-        cmd_str = f'tasklist /FI "PID eq {pid}"'
-        result = run_shell_cmd(cmd_str, suppress_output=True)
-        return str(pid) in result.stdout
+        return psutil.pid_exists(pid)
     except Exception as e:
         logger.error(f"Error checking if server is running: {e}")
         return False
+
+
+def get_parent_pid_from_child(child_pid: int) -> int | None:
+    """
+    Retrieves the parent process ID of a given child process ID.
+
+    :param child_pid: The process ID of the child process.
+    :return: The process ID of the parent process, or None if not found.
+    """
+    try:
+        child_process = psutil.Process(child_pid)
+        parent_process = child_process.parent()
+        if parent_process is None:
+            logger.debug(f"No parent process found for child PID {child_pid}")
+            return None
+        return parent_process.pid
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+        logger.error(f"Error retrieving parent process for PID {child_pid}")
+        return None
 
 
 def generate_batch_file() -> str:
